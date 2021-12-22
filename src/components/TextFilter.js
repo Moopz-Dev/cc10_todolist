c82c36f5d9162292941b4d7cd0bc34d73257ce99
Moopz-Dev <pongzthor@gmail.com>
2021-12-22 08:14:02 +0700

    tacticalpause

--- src/components/TextFilter.js
+++ src/components/TextFilter.js
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
-
-function TextFilter({ setSearchText }) {
-	const [input, setInput] = useState("");
-
-	useEffect(() => {
-		const timeoutId = setTimeout(() => {
-			setSearchText(input);
-			console.log("input changed");
-		}, 1500);
-		return () => {
-			clearTimeout(timeoutId);
-		};
-	}, [input, setSearchText]);
-
-	const handleChangeText = event => {
-		setInput(event.target.value);
-	};
-
+function TextFilter() {
 	return (
 		<div className="input-group">
-			<input
-				type="text"
-				className="form-control rounded-0"
-				onChange={handleChangeText}
-				value={input}
-			/>
-			<button
-				className="btn btn-secondary rounded-0"
-				onClick={() => setSearchText}>
+			<input type="text" className="form-control rounded-0" />
+			<button className="btn btn-secondary rounded-0">
 				<i className="fas fa-times"> </i>
 			</button>
 		</div>
